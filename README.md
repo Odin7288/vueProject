ddc8941ebeaf639a7b178305f6964756f7da3b0b
Odin Yao <odinyuu@gmail.com>
2022-03-30 14:22:18 +0800

    add babelrc

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Vue 
 ## 30-03-2022
 ## node.js 16 , ....
+
+`````
+npx babel src\lab1_var_let_const.js
+`````
---
+++ src/lab1_var_let_const.js
@@ -0,0 +1,8 @@
+let x = 5
+console.log(x)
+x += 1
+console.log(x)
+const y = 'Hello World x =${x}'
+console.log(y)
+const z = `Hello World x= ${x}`
+console.log(z) 
